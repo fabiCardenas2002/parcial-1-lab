dbbabf7afa3dbe9a7e765eba1dffd7b467ad6882
fabiCardenas2002 <145080027+fabiCardenas2002@users.noreply.github.com>
2023-10-06 09:21:27 -0500

    Add files via upload

---
+++ parcial/src/parcial/Vehículo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parcial;
+
+/**
+ *
+ * @author DELL I3
+ */
+public class Vehículo {
+    String marca;
+    String modelo;
+    int año;
+    String propietario;        
+    String placa;     
+    
+    
+    //metodo 
+     public void mostrarInformacion(){
+         system.out.prin
+     }
+    
+}
